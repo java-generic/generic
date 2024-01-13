437de6fd409f325d5f66056b9ebb528c5b1d75e5
GwangHun Moon <mkh9293@gmail.com>
2024-01-13 20:04:42 +0900

    sample code commit

---
+++ src/main/java/org/example/basic/Basic5.java
@@ -0,0 +1,18 @@
+package org.example.basic;
+
+/**
+ * 제네릭 인터페이스
+ */
+public class Basic5 {
+    public static void main(String[] args) {
+        IAdd<Integer> o1 = (x, y) -> x + y;
+
+        int result = o1.add(10, 10);
+        System.out.println(result);
+    }
+
+    private interface IAdd<T> {
+        public T add(T x, T y);
+    }
+
+}
