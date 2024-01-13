626d50ec477c4995a32d9b83f7e0ed3d21978167
CodingWon <kingcoding2298@gmail.com>
2024-01-13 16:47:22 +0900

    feat(test): 자바에서 제네릭 개념 이해하기 #1
    작업 완료
    
    Closes #1

--- src/main/java/org/example/ex01/Exercise.java
+++ src/main/java/org/example/ex01/Exercise.java
@@ -3,6 +3,6 @@ package org.example.ex01;
 public class Exercise {
     public static void main(String[] args) {
         System.out.println("hellow Exercise");
-        System.out.println("hellow");
+        System.out.println("hellow commit");
     }
 }
